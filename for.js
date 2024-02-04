5d69887d75ae114aadbebc133a4fdbfb5e951085
kadir-zeren <k4dzeren@gmail.com>
2024-02-04 18:28:39 +0300

    for random number

--- for.js
+++ for.js
@@ -1,12 +1,19 @@
-for (let i = 1; i <= 100; i++) {
-  console.log(`${i}-2024`);
-}
+// for (let i = 1; i <= 100; i++) {
+//   console.log(`${i}-2024`);
+// }
 
-const n = prompt("enter your number");
-let sum = 0;
+// const n = prompt("enter your number");
+// let sum = 0;
 
-for (let i = 1; i <= n; i++) {
-  sum = sum + i;
-  console.log(sum);
+// for (let i = 1; i <= n; i++) {
+//   sum = sum + i;
+//   console.log(sum);
+// }
+// console.log(`SUM: ${sum}`);
+
+const number = prompt("Kac adet sayi uretmek istersiniz");
+
+for (let sayac = 1; sayac <= number; sayac++) {
+  const rasgeleSayi = Math.round(Math.random() * 100);
+  console.log(`${sayac}. sayiniz : ${rasgeleSayi}`);
 }
-console.log(`SUM: ${sum}`);
