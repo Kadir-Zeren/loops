1bd627b1addb69757d8349e99a17f34547a931be
kadir-zeren <k4dzeren@gmail.com>
2024-02-04 14:22:01 +0300

    for

--- for.js
+++ for.js
@@ -1,1 +1,12 @@
-for(l)
+for (let i = 1; i <= 100; i++) {
+  console.log(`${i}-2024`);
+}
+
+const n = prompt("enter your number");
+let sum = 0;
+
+for (let i = 1; i <= n; i++) {
+  sum = sum + i;
+  console.log(sum);
+}
+console.log(`SUM: ${sum}`);
